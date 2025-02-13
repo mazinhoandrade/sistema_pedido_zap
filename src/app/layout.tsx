c05e2d21b0755c937e6f10fb00568fe23929d9f4
mazinhoandrade <irismarandrade94@gmail.com>
2025-02-13 16:34:18 -0300

    Resolvendo conflitos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ThemeProvider } from "@/components/Theme-Provider";
-import { Toaster } from "@/components/ui/toaster"
+import { ThemeProvider } from "../components/theme-provider";
+import { Toaster } from "../components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { Footer } from '@/components/Footer'
-import { Header } from '@/components/Header'
+import { Footer } from '../components/Footer'
+import { Header } from '../components/Header'
 import { ProductSkeleton } from '@/components/products/Skeleton'
 import { ProductsTab } from '@/components/products/Tab'
 
